099dfed792a3bed4bbc8874f131a1ddf0f612eb5
Vladimir Galicia <thewakosmanclub@gmail.com>
2019-10-23 18:08:05 -0500

    add apps

--- api/settings.py
+++ api/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Libraries
+    'rest_framework',
+    # apps 
+    'tasks'
 ]
 
 MIDDLEWARE = [
