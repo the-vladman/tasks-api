9329beeb93e2015e543b2f37f341e781b68fdfe8
Vladimir Galicia <thewakosmanclub@gmail.com>
2019-10-23 18:05:11 -0500

    Task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+PENDING = 0
+COMPLETED = 1
+
+TASK_STATUS = (
+    (PENDING, 'pending'),
+    (COMPLETED, 'completed'),
+)
+
+class Task(models.Model):
+    description = models.CharField(max_length=250)
+    duration = models.PositiveSmallIntegerField()
+    recorded_time = models.PositiveSmallIntegerField(null=True, blank=True)
+    status = models.PositiveSmallIntegerField(choices=TASK_STATUS, default=0)
