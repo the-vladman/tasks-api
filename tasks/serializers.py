9f486c42d53066585206d6902ca4b80cadd4bc97
Vladimir Galica <thewakosmanclub@gmail.com>
2019-10-23 23:31:05 -0500

    cannot update on completed status

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 from .models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = ['id', 'description', 'duration', 'recorded_time', 'status']
+    
+    def validate(self, data):
+        if self.instance.status == 1:
+            raise ValidationError('task completed')
+        return data
