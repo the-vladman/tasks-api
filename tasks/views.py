c73eb4f7526fe228591c9de5e6f8f57706766682
Vladimir Galica <thewakosmanclub@gmail.com>
2019-10-24 00:31:31 -0500

    search on description

--- tasks/views.py
+++ tasks/views.py
@@ -2,9 +2,11 @@ from rest_framework import viewsets
 from .models import Task
 from .serializers import TaskSerializer
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 class TaskViewset(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_fields = ['status']
+    search_fields = ['description']
